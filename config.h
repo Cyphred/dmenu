7eaf82471138ee44600279c6f6ae8d1a843eb6e7
Jeremy Andrews Zantua <jeremyzantua@gmail.com>
2021-09-20 01:10:40 +0800

    Updated colors

--- config.h
+++ config.h
@@ -4,14 +4,14 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"Noto Sans:size=8"
+	"Noto Sans:size=9"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#9fafaf", "#222222" },
-	[SchemeSel] = { "#222222", "#cb712a" },
-	[SchemeOut] = { "#222222", "#9fafaf" },
+	[SchemeNorm] = { "#686966", "#eae1c0" },
+	[SchemeSel] = { "#eae1c0", "#cb712a" },
+	[SchemeOut] = { "#eae1c0", "#9fafaf" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
